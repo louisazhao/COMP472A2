a6fac82f047a857410fc1cc2cfec9deb4d2c5c0e
Melodie Thibeault <melothibeault@gmail.com>
2018-11-15 09:45:02 -0500

    DecisionTree J48, TODO: rename old decision tree for RandomForestML

--- src/DecisionTreeJ48.java
+++ src/DecisionTreeJ48.java
@@ -2,23 +2,23 @@ import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
 
-public class DecisionTree extends MachineLearningModel {
+public class DecisionTreeJ48 extends MachineLearningModel {
 
     private J48 decisionTree;
 
-    public DecisionTree(FileProcess trainingData, FileProcess validationData, FileProcess testSet) {
+    public DecisionTreeJ48(FileProcess trainingData, FileProcess validationData, FileProcess testSet) {
         super(trainingData, validationData, testSet);
     }
 
     /**
      * @param fileName The function will train the model using the filtered training set, and then save the trained model into a .model file
      */
-    public void trainAndSaveModel(String fileName) {
+    public void trainAndSaveModel(String fileName, float confidenceFactor, int minNumObj) {
         decisionTree = new J48();
-        //TODO call set functions to change hyper-parameters, may need another function if 2 dataset using different hyper-parameters
+        decisionTree.setConfidenceFactor(confidenceFactor);
+        decisionTree.setMinNumObj(minNumObj);
         try {
             Instances filteredTrainingSet = applyTwoFiltersOnDataSet(getTrainingSet());
-
             decisionTree.buildClassifier(filteredTrainingSet);
             SerializationHelper.write(fileName, decisionTree);
         } catch (Exception e) {
--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,27 @@ public class Main {
         NaiveBayesML naiveBayesData1 = new NaiveBayesML(trainingData1, validationData1,testSet1);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
         //naiveBayesData1.trainAndSaveModel("OutputFiles/ds1/naiveBayes1.model");
-        naiveBayesData1.evaluateModelWithValidationSet("OutputFiles/ds1/naiveBayes1.model", "OutputFiles/ds1/ds1Val-nb.csv");
-        naiveBayesData1.classifyNewData("OutputFiles/ds1/naiveBayes1.model", "OutputFiles/ds1/ds1Test-nb.csv");
+        //naiveBayesData1.evaluateModelWithValidationSet("OutputFiles/ds1/naiveBayes1.model", "OutputFiles/ds1/ds1Val-nb.csv");
+        //naiveBayesData1.classifyNewData("OutputFiles/ds1/naiveBayes1.model", "OutputFiles/ds1/ds1Test-nb.csv");
 
 
-
-        DecisionTree decisionTreeData1 = new DecisionTree(trainingData1, validationData1,testSet1);
+        DecisionTreeJ48 decisionTreeData1 = new DecisionTreeJ48(trainingData1, validationData1,testSet1);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
-        //decisionTreeData1.trainAndSaveModel("OutputFiles/ds1/decisionTree1.model");
+        decisionTreeData1.trainAndSaveModel("OutputFiles/ds1/decisionTree1.model", 0.2f, 3);
         decisionTreeData1.evaluateModelWithValidationSet("OutputFiles/ds1/decisionTree1.model", "OutputFiles/ds1/ds1Val-dt.csv");
         decisionTreeData1.classifyNewData("OutputFiles/ds1/decisionTree1.model", "OutputFiles/ds1/ds1Test-dt.csv");
 
         MultilayerPerceptronML multilayerPerceptronML1=new MultilayerPerceptronML(trainingData1,validationData1,testSet1);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
         //multilayerPerceptronML1.trainAndSaveModel("OutputFiles/ds1/multilayerPerceptron1.model");
-        multilayerPerceptronML1.evaluateModelWithValidationSet("OutputFiles/ds1/multilayerPerceptron1.model","OutputFiles/ds1/ds1Val-3.csv");
-        multilayerPerceptronML1.classifyNewData("OutputFiles/ds1/multilayerPerceptron1.model","OutputFiles/ds1/ds1Test-3.csv");
+        //multilayerPerceptronML1.evaluateModelWithValidationSet("OutputFiles/ds1/multilayerPerceptron1.model","OutputFiles/ds1/ds1Val-3.csv");
+        //multilayerPerceptronML1.classifyNewData("OutputFiles/ds1/multilayerPerceptron1.model","OutputFiles/ds1/ds1Test-3.csv");
+
+        RandomForestML randomForestData1 = new RandomForestML(trainingData1, validationData1, testSet1);
+        //comment out the following line to see the saved model actually works, you need to run the line at least once
+        randomForestData1.trainAndSaveModel("OutputFiles/ds1/randomForest1.model");
+        randomForestData1.evaluateModelWithValidationSet("OutputFiles/ds1/randomForest1.model", "OutputFiles/ds1/ds1Val-rf.csv");
+        randomForestData1.classifyNewData("OutputFiles/ds1/randomForest1.model", "OutputFiles/ds1/ds1Test-rf.csv");
 
 
         /********        dataset2         ******/
@@ -50,13 +55,13 @@ public class Main {
         NaiveBayesML naiveBayesData2 = new NaiveBayesML(trainingData2, validationData2,testSet2);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
         //naiveBayesData2.trainAndSaveModel("OutputFiles/ds2/naiveBayes2.model");
-        naiveBayesData2.evaluateModelWithValidationSet("OutputFiles/ds2/naiveBayes2.model", "OutputFiles/ds2/ds2Val-nb.csv");
-        naiveBayesData2.classifyNewData("OutputFiles/ds2/naiveBayes2.model", "OutputFiles/ds2/ds2Test-nb.csv");
+        //naiveBayesData2.evaluateModelWithValidationSet("OutputFiles/ds2/naiveBayes2.model", "OutputFiles/ds2/ds2Val-nb.csv");
+        //naiveBayesData2.classifyNewData("OutputFiles/ds2/naiveBayes2.model", "OutputFiles/ds2/ds2Test-nb.csv");
 
 
-        DecisionTree decisionTreeData2 = new DecisionTree(trainingData2, validationData2,testSet2);
+        DecisionTreeJ48 decisionTreeData2 = new DecisionTreeJ48(trainingData2, validationData2,testSet2);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
-        //decisionTreeData2.trainAndSaveModel("OutputFiles/ds2/decisionTree2.model");
+        decisionTreeData2.trainAndSaveModel("OutputFiles/ds2/decisionTree2.model", 0.1f, 4);
         decisionTreeData2.evaluateModelWithValidationSet("OutputFiles/ds2/decisionTree2.model", "OutputFiles/ds2/ds2Val-dt.csv");
         decisionTreeData2.classifyNewData("OutputFiles/ds2/decisionTree2.model", "OutputFiles/ds2/ds2Test-dt.csv");
 
@@ -64,7 +69,13 @@ public class Main {
         MultilayerPerceptronML multilayerPerceptronML2=new MultilayerPerceptronML(trainingData2,validationData2,testSet2);
         //comment out the following line to see the saved model actually works, you need to run the line at least once
         //multilayerPerceptronML2.trainAndSaveModel("OutputFiles/ds2/multilayerPerceptron2.model");
-        multilayerPerceptronML2.evaluateModelWithValidationSet("OutputFiles/ds2/multilayerPerceptron2.model","OutputFiles/ds2/ds2Val-3.csv");
-        multilayerPerceptronML2.classifyNewData("OutputFiles/ds2/multilayerPerceptron2.model","OutputFiles/ds2/ds2Test-3.csv");
+        //multilayerPerceptronML2.evaluateModelWithValidationSet("OutputFiles/ds2/multilayerPerceptron2.model","OutputFiles/ds2/ds2Val-3.csv");
+        //multilayerPerceptronML2.classifyNewData("OutputFiles/ds2/multilayerPerceptron2.model","OutputFiles/ds2/ds2Test-3.csv");
+
+        RandomForestML randomForestData2 = new RandomForestML(trainingData2, validationData2, testSet2);
+        //comment out the following line to see the saved model actually works, you need to run the line at least once
+        randomForestData2.trainAndSaveModel("OutputFiles/ds2/randomForest2.model");
+        randomForestData2.evaluateModelWithValidationSet("OutputFiles/ds2/randomForest2.model", "OutputFiles/ds1/ds2Val-rf.csv");
+        randomForestData2.classifyNewData("OutputFiles/ds2/randomForest2.model", "OutputFiles/ds2/ds2Test-rf.csv");
     }
 }
